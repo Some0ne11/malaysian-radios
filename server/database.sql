0e1f718725dc68f517a959f68c490cc36239adfc
Amrdanial_ <ammardanial274@gmail.com>
2026-08-15 16:43:24 +0800

    minor change

--- server/database.sql
+++ server/database.sql
@@ -10,16 +10,3 @@ CREATE TABLE IF NOT EXISTS stations (
   clear_keys TEXT, -- Store as JSON string, e.g., '[{"keyid": "...", "key": "..."}]'
   subscription_type TEXT DEFAULT 'FREE'
 );
-
--- Example Insert based on previous draft
--- INSERT INTO stations (id, name, category_id, stream_url, fallback_urls, logo_url, status, clear_keys, subscription_type)
--- VALUES (
---   'SuriaFM',
---   'SURIA FM',
---   'RADIO',
---   'https://playerservices.streamtheworld.com/api/livestream-redirect/SURIA_FMAAC',
---   'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Suria_logo-01.png/1280px-Suria_logo-01.png',
---   'working',
---   '[{"keyid": "", "key": ""}]',
---   'FREE'
--- );
