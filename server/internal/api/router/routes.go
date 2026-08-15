6469ea7326e40527d40ea5d95065b2e4bff79342
Amrdanial_ <ammardanial274@gmail.com>
2026-08-15 19:41:25 +0800

    minor format change

--- server/internal/api/router/routes.go
+++ server/internal/api/router/routes.go
@@ -4,9 +4,18 @@ import (
 	"github.com/go-chi/chi/v5"
 
 	"malaysian-radios-server/internal/api/handler"
+	"malaysian-radios-server/internal/middleware"
 )
 
 // SetupRoutes registers all API endpoints onto the router
 func SetupRoutes(r chi.Router) {
-	r.Get("/api/stations", handler.GetStationsHandler)
+	// Public endpoint to generate token using CLIENT_SECRET
+	r.Get("/api/token", middleware.GetTokenHandler)
+
+	// Protected by token middleware
+	r.Route("/api/stations", func(r chi.Router) {
+		r.Use(middleware.TokenAuthMiddleware) // Applies to all routes in this group
+		r.Get("/", handler.GetStationsHandler)       // Maps to GET /api/stations
+		r.Get("/{id}", handler.GetStationByIDHandler) // Maps to GET /api/stations/{id}
+	})
 }
